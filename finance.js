d775225e9cd5ad263d71a392bbcda5b2c2de8879
Viki W <oysters109@163.com>
2014-01-02 23:22:11 +0800

    v0.1

--- finance.js
+++ finance.js
@@ -2,28 +2,71 @@ var app = angular.module('finance', []);
 app.controller('ShowCtrl',['$scope', '$http','$filter',
 	function ShowCtrl($scope, $http, $filter){
 		$scope.moneys = [];
-		$scope.today = $filter('date')(new Date(),'yyyy/MM/dd');
-	  $scope.cancel = function (){
-	   $scope.item = '';
-	   $scope.amount = '';
-	   $scope.payment = '';	  	
-	  };
-	  getdata();
-	  $scope.save = function (item, amount, payment, today){
-	  	var money = {'item': item, 'amount': amount, 'payment' : payment, 'adddate' : today};
-	  	$http.post('/new', money).
-	  	success(function(data){
-	  	  $scope.moneys.push(data);
-	  	  $scope.item = '';
-	  	  $scope.amount = '';
-	  	  $scope.payment = '';
+		$scope.adddate = $filter('date')(new Date(),'yyyy/MM/dd');
+		$scope.payment = 'Cash';
+		$scope.searchType = '';
+		
+		$scope.cancel = function (){
+			$scope.item = '';
+			$scope.amount = '';
+			$scope.payment = '';	  	
+		};
+		
+		$scope.clear = function (){
+			$scope.keyword = '';
+			$scope.startDate = '';
+			$scope.endDate = '';
+			$scope.searchType = '';	
+		};		
+		
+		$scope.save = function (item, amount, payment, today){
+			var money = {
+				'item': item, 
+				'amount': amount, 
+				'payment' : payment, 
+				'adddate' : today
+			};
+			$http.post('/new', money).
+			success(function(data){
+				$scope.moneys.push(data);
+				$scope.item = '';
+				$scope.amount = '';
+				$scope.payment = '';
 	  		});	  	
-	  };
+		};
 	  
-	  function getdata(){
-	  	$http.get('/getdata').
-	  	success(function(data){
-	  		$scope.moneys = data;
-	  	});
-	  };		    	  
-	}]);
+    	getdata();
+		function getdata(){
+			$http.get('/getdata').
+			success(function(data){
+				$scope.moneys = data;
+			});
+		};	
+	}
+]);
+
+app.filter('daterange', function ()
+{
+  return function(items,startDate, endDate)
+	{
+		var result = [];
+
+		var startDate = (startDate && !isNaN(Date.parse(startDate))) ? Date.parse(startDate) : 0;
+		var endDate = (endDate && !isNaN(Date.parse(endDate))) ? Date.parse(endDate) : new Date().getTime();
+ 
+		if (items && items.length > 0)
+		{
+			angular.forEach(items, function (item)
+			{
+				var itemDate = new Date(item.adddate);
+ 
+				if (itemDate >=startDate && itemDate <= endDate)
+				{
+					result.push(item);
+				}
+			});
+ 
+			return result;
+		}
+	};
+});
